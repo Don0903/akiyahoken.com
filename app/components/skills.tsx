45de980f13f08787c3b801332653fed811738e78
Don0903 <hoken0903@gmail.com>
2023-07-27 19:15:38 +0200

    speedup

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -21,8 +21,8 @@ export default function skills() {
       opacity: 1,
       scale: 1,
       transition: {
-        delayChildren: 0.3,
-        staggerChildren: 0.2,
+        delayChildren: 0.1,
+        staggerChildren: 0.1,
       },
     },
   };
