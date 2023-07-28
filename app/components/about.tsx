270cbed9bd33b0da5e8d98d4b422652337982e21
Don0903 <hoken0903@gmail.com>
2023-07-28 02:20:38 +0200

    about me animaitons

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,9 +1,17 @@
+"use client";
+import { motion } from "framer-motion";
+
 export default function About() {
   return (
     <div className="snap-center w-full h-screen flex flex-col justify-center items-center snap-mandatory">
-      <div className="flex">
-        <h1 className="text-4xl text-center">About me page in development</h1>
-      </div>
+      <motion.div
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className=" text-4xl text-center"
+      >
+        About me page in development
+      </motion.div>
     </div>
   );
 }
