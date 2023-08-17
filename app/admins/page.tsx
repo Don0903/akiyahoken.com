3b7a1e7a41e085b2b708398e6aea3484050d211b
Don0903 <hoken0903@gmail.com>
2023-08-17 13:41:53 +0200

    apo stuff

--- app/admins/page.tsx
+++ app/admins/page.tsx
@@ -17,14 +17,14 @@ export default async function AdminsPage() {
       <div className="w-full h-screen flex flex-col items-center justify-center px-4">
         <div className="w-full flex h-fit shadow bg-gray-200 flex-col items-center justify-center rounded-md">
           <h1 className="text-4xl font-bold">Admins</h1>
-          <p>You're {session.user.name}, so you have access</p>
+          <p>You are {session.user.name}, so you have access</p>
           <div className="w-full flex items-center justify-center">
             <div className=""></div>
 
             {/* Additional content for admin users only*/}
             {session.user.role === "superadmin" && (
               <div className="bg-blue-100 p-4 rounded-md mt-4">
-                <p>You're a Superadmin! Here's some extra content.</p>
+                <p>You are a Superadmin! Here are some extra content.</p>
               </div>
             )}
           </div>
--- app/denied/page.tsx
+++ app/denied/page.tsx
@@ -6,7 +6,7 @@ export default function Denied() {
         <h1 className="text-4xl">Access Denied</h1>
         <p className="text-center ">
           You are logged in, but do not have permission to access this page yet. <br/>
-          Request to access has been sent to the Admins - we'll come back to you
+          Request to access has been sent to the Admins - we will come back to you
           shortly!
         </p>
       </div>
